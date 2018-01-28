8c56cf0a1753de547b5b3aef4471be2b7f278680
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2018-01-28 22:19:16 +0200

    Edit

--- app/src/main/java/com/example/rabee/breath/Adapters/UserListAdapter.java
+++ app/src/main/java/com/example/rabee/breath/Adapters/UserListAdapter.java
@@ -1,17 +1,21 @@
 package com.example.rabee.breath.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.rabee.breath.Activities.UserProfileActivity;
 import com.example.rabee.breath.GeneralInfo;
 import com.example.rabee.breath.Models.UserModel;
 import com.example.rabee.breath.R;
+import com.example.rabee.breath.Services.FollowingService;
 import com.squareup.picasso.Picasso;
 
+import java.net.MalformedURLException;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -24,6 +28,8 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserVi
     public View view;
     private Context mContext;
     public static List<UserModel> userModelList;
+    CircleImageView ivProfile;
+    TextView tvName;
 
 
     public UserListAdapter(Context mContext, List<UserModel> userModelList) {
@@ -34,17 +40,45 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserVi
     @Override
     public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         view = LayoutInflater.from(mContext).inflate(R.layout.user_list_view_item, null);
-            vv
-
+        ivProfile=(CircleImageView)view.findViewById(R.id.profile_pic);
         return new UserViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(UserViewHolder holder, int position) {
-        String fullName=userModelList.get(position).getFirst_name() +""+userModelList.get(position).getLast_name();
+    public void onBindViewHolder(UserViewHolder holder, final int position) {
+        UserModel userModel=new UserModel();
+        userModel=userModelList.get(position);
+        String fullName=userModel.getFirst_name() +" "+userModel.getLast_name();
+
         holder.tvName.setText(fullName);
         String imageUrl = GeneralInfo.SPRING_URL + "/" +userModelList.get(position).getImage() ;
         Picasso.with(mContext).load(imageUrl).into(holder.ivProfile);
+        final UserModel finalUserModel = userModel;
+        holder.ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (userModelList.get(position).getId() == GeneralInfo.getUserID()) {
+                    Intent i = new Intent(mContext, UserProfileActivity.class);
+                    mContext.startActivity(i);
+                } else {
+                    FollowingService.startRightActivity(mContext, userModelList.get(position).getFirst_name(), userModelList.get(position).getId(), finalUserModel.getImage());
+                }
+            }
+        });
+        holder.tvName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (userModelList.get(position).getId() == GeneralInfo.getUserID()) {
+                    Intent i = new Intent(mContext, UserProfileActivity.class);
+                    mContext.startActivity(i);
+                } else {
+                    FollowingService.startRightActivity(mContext, userModelList.get(position).getFirst_name(), userModelList.get(position).getId(), finalUserModel.getImage());
+                }
+            }
+        });
+
     }
 
 
